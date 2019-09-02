b1e987585aaee6edd2e002d5e98407ed91c271c0
fish-ball <57082212@qq.com>
2019-09-02 18:03:38 +0800

    强化功能

--- components/_modules/index.js
+++ components/_modules/index.js
@@ -4,8 +4,9 @@ import MOptionForm from './OptionForm.vue'
 import MListView from './ListView.vue'
 import MListViewTable from './ListViewTable.vue'
 import MEditView from './EditView.vue'
+import MEmptyView from './EmptyView.vue'
 
 export const OptionForm = MOptionForm
 export const ListView = MListView
 export const ListViewTable = MListViewTable
-export const EditView = MEditView
+export const EditView = MEmptyView
